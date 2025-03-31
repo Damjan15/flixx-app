b9d3b65da95ba84ce26e92de5597a30c035b7890
Damjan15 <stamenovdamjan5@gmail.com>
2025-04-01 00:58:10 +0200

    feat: getting & displaying popular movies

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ function init() {
   switch (global.currentPage) {
     case "/":
     case "/index.html":
-      console.log("Home");
+      displayPopularMovies();
       break;
     case "/shows.html":
       console.log("Shows");
@@ -33,6 +33,54 @@ function highlightActiveLink() {
     if (link.getAttribute("href") === global.currentPage) {
       link.classList.add("active");
     }
+  });
+}
+
+// Fetch Data from API
+async function fetchAPIData(endpoint) {
+  const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+  const API_URL = "https://api.themoviedb.org/3/";
+
+  const res = await fetch(
+    `${API_URL}${endpoint}?api_key=${API_KEY}&language=en-US`
+  );
+
+  const data = await res.json();
+
+  return data;
+}
+
+// Display Popular Movies
+async function displayPopularMovies() {
+  const { results } = await fetchAPIData("movie/popular");
+
+  results.forEach((movie) => {
+    const div = document.createElement("div");
+    div.classList.add("card");
+
+    div.innerHTML = `<a href="movie-details.html?id=${movie.id}">
+            ${
+              movie.poster_path
+                ? `<img
+              src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
+              class="card-img-top"
+              alt="${movie.title}"
+            />`
+                : `<img
+            src="../images/no-image.jpg"
+            class="card-img-top"
+            alt="${movie.title}"
+          />`
+            }
+          </a>
+          <div class="card-body">
+            <h5 class="card-title">${movie.title}</h5>
+            <p class="card-text">
+              <small class="text-muted">Release: ${movie.release_date}</small>
+            </p>
+          </div>`;
+
+    document.querySelector("#popular-movies").appendChild(div);
   });
 }
 
