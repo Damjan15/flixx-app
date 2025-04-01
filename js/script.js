8704a191e49923aa6fc888fbd695408a6e02e1d7
Damjan15 <stamenovdamjan5@gmail.com>
2025-04-01 10:39:27 +0200

    feat: overlay for TV & Movies added

--- js/script.js
+++ js/script.js
@@ -127,10 +127,10 @@ async function displayPopularShows() {
 
 async function displayMovieDetails() {
   const movieId = window.location.search.split("=")[1];
-  console.log(movieId);
 
   const movie = await fetchAPIData(`movie/${movieId}`);
-  console.log(movie);
+
+  displayBackgroundImage("movie", movie.backdrop_path);
 
   const div = document.createElement("div");
 
@@ -206,6 +206,27 @@ function hideSpinner() {
 
 function addCommasToNumber(number) {
   return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+function displayBackgroundImage(type, backgroundPath) {
+  const overlayDiv = document.createElement("div");
+  overlayDiv.style.backgroundImage = `url(https://image.tmdb.org/t/p/original/${backgroundPath})`;
+  overlayDiv.style.backgroundSize = "cover";
+  overlayDiv.style.backgroundPosition = "center";
+  overlayDiv.style.backgroundRepeat = "no-repeat";
+  overlayDiv.style.height = "100vh";
+  overlayDiv.style.width = "100vw";
+  overlayDiv.style.position = "absolute";
+  overlayDiv.style.top = "0";
+  overlayDiv.style.left = "0";
+  overlayDiv.style.zIndex = "-1";
+  overlayDiv.style.opacity = "0.1";
+
+  if (type === "movie") {
+    document.querySelector("#movie-details").appendChild(overlayDiv);
+  } else {
+    document.querySelector("#show-details").appendChild(overlayDiv);
+  }
 }
 
 document.addEventListener("DOMContentLoaded", init);
